87bff732635c1b6a936a0a58906fab887cf07550
LeonardTarigan <leontrg32@gmail.com>
2023-10-23 14:11:22 +0700

    mengisi konten program kerja

--- src/components/program-kerja/ProgramKerja.tsx
+++ src/components/program-kerja/ProgramKerja.tsx
@@ -1,6 +1,8 @@
 import type { FC } from "react";
 import Carousel from "../Carousel";
 import { Card } from "../Card";
+import { programKerja } from "./data-program-kerja";
+import Image from "next/image";
 
 const ProgramKerja: FC = () => {
   return (
@@ -12,21 +14,25 @@ const ProgramKerja: FC = () => {
         options={{ dragFree: false, align: "start" }}
         containerClassName="lg:px-56"
       >
-        <Card className="flex-[0_0_100%] pl-[20px]" innerBox="p-7">
-          <div className="relative w-full aspect-video bg-gray-500 rounded-md"></div>
-        </Card>
-        <Card className="flex-[0_0_100%] pl-[20px]" innerBox="p-7">
-          <div className="relative w-full aspect-video bg-gray-500 rounded-md"></div>
-        </Card>
-        <Card className="flex-[0_0_100%] pl-[20px]" innerBox="p-7">
-          <div className="relative w-full aspect-video bg-gray-500 rounded-md"></div>
-        </Card>
-        <Card className="flex-[0_0_100%] pl-[20px]" innerBox="p-7">
-          <div className="relative w-full aspect-video bg-gray-500 rounded-md"></div>
-        </Card>
-        <Card className="flex-[0_0_100%] pl-[20px]" innerBox="p-7">
-          <div className="relative w-full aspect-video bg-gray-500 rounded-md"></div>
-        </Card>
+        {programKerja.map(({ description, imagePath }, index) => {
+          return (
+            <Card
+              key={index}
+              className="flex-[0_0_100%] pl-[20px]"
+              innerBox="p-7"
+              deskripsi={description}
+            >
+              <div className="relative w-full h-56 overflow-hidden bg-gray-500 rounded-md">
+                <Image
+                  src={imagePath}
+                  alt="Pic"
+                  fill={true}
+                  className="object-cover"
+                />
+              </div>
+            </Card>
+          );
+        })}
       </Carousel>
     </>
   );
---
+++ src/components/program-kerja/data-program-kerja.ts
@@ -0,0 +1,12 @@
+export const programKerja = [
+  {
+    imagePath: "/proker-cosmos.png",
+    description:
+      "COSMOS - Celebrating Open Source Movement with POROS adalah perayaan software freedom day dengan adanya seminar dan workshop CTF.",
+  },
+  {
+    imagePath: "/proker-codex.png",
+    description:
+      "CodEx (Code and Exploitation) - Coming soon! Colaboration with BCC",
+  },
+];
