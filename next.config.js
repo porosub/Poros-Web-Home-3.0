16e27dd0a99900b86bdfe396b2b0ae4b774380e0
KevinDanudoro <kevindanudoro68@gmail.com>
2023-10-24 14:05:07 +0700

    update: bug pada card tidak muncul, merubah next config ke pengaturan semula

--- next.config.js
+++ next.config.js
@@ -1,16 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  images: {
-    unoptimized: true,
-    remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "placehold.co",
-        port: "",
-        pathname: "**",
-      },
-    ],
-  },
+  output: "export",
 };
 
 module.exports = nextConfig;
