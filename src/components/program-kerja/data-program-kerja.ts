69165baa153146660f5519c84abc0dd97534378b
LeonardTarigan <leontrg32@gmail.com>
2023-10-25 13:08:54 +0700

    mengisi konten program kerja

---
+++ src/components/program-kerja/data-program-kerja.ts
@@ -0,0 +1,17 @@
+interface ProkerProps {
+  imagePath: string;
+  description: string;
+}
+
+export const programKerja: ProkerProps[] = [
+  {
+    imagePath: "/proker-cosmos.png",
+    description:
+      "COSMOS - Celebrating Open Source Movement with POROS adalah perayaan software freedom day dengan adanya seminar dan workshop CTF.",
+  },
+  {
+    imagePath: "/proker-codex.png",
+    description:
+      "CodEx (Code and Exploitation) - Coming soon! Colaboration with BCC",
+  },
+];
