dc321fd477d3110cc7824a32578459b7859960e1
radzag7127 <radzaglagah@gmail.com>
2023-10-28 08:39:16 +0700

    pengisian konten 28-10-2023

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -4,17 +4,21 @@ interface CardProps extends React.HTMLAttributes<HTMLDivElement> {
   children?: React.ReactNode;
   deskripsi?: string;
   innerBox?: string;
+  gambar?: string;
 }
 
 export const Card: FC<CardProps> = (props) => {
-  const { children, deskripsi, innerBox = "p-5", ...resprops } = props;
+  const { gambar, children, deskripsi, innerBox = "p-5", ...resprops } = props;
   return (
     <div {...resprops}>
       <div
         className={`rounded-2xl bg-gray-200 flex items-center flex-col ${innerBox}`}
       >
         {children ?? (
-          <div className="w-full aspect-video md:aspect-square bg-gray-500 rounded-lg"></div>
+          <img
+            src={gambar}
+            className="w-full aspect-video md:aspect-square rounded-lg"
+          />
         )}
         <p className="mt-5 self-start">
           {deskripsi ?? "lorem iplsu dolor sit amet"}
---
+++ src/components/pencapaian/data-pencapaian.tsx
@@ -0,0 +1,14 @@
+export const dataPencapaian = [
+  {
+    gambar: "/pencapaian-gambar-1.svg",
+    deskripsi: "COMPFEST 15 CTF 1st Runner Up - 'Tim AcRtf'.",
+  },
+  {
+    gambar: "/pencapaian-gambar-2.svg",
+    deskripsi: "JOINTS 2023 CTF 3rd Place - Tim 'Yoto Curry'.  ",
+  },
+  {
+    gambar: "/pencapaian-gambar-3.svg",
+    deskripsi: "TECCHOMFEST 2023 CTF 3rd Place - Tim 'AcRtf'.  ",
+  },
+];
