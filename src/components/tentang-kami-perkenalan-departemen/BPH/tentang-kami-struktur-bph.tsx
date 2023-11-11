f9470207840f292a54f223ba559c3f87f9484d54
radzag7127 <radzaglagah@gmail.com>
2023-11-11 11:22:25 +0700

    11-11-2023 11.22 bph

--- src/components/tentang-kami-perkenalan-departemen/BPH/tentang-kami-struktur-bph.tsx
+++ src/components/tentang-kami-perkenalan-departemen/BPH/tentang-kami-struktur-bph.tsx
@@ -10,10 +10,10 @@ const TentangKamiStrukturBPH = () => {
   return (
     <div>
       <div className="flex flex-col items-center my-4 mb-24">
-        <h2 className="font-rubik lg:text-3xl font-bold text-xl text-[#212C4F] mb-2 lg:w-[1100px]">
+        <h2 className="font-rubik lg:text-3xl md:text-2xl font-bold text-xl text-[#212C4F] mb-2 lg:w-[800px] xl:w-[1100px]">
           Struktur BPH
         </h2>
-        <div className="w-[330px] lg:w-[1100px] h-[3px] bg-[#C3C3C3]"></div>
+        <div className="w-[330px] lg:w-[800px] xl:w-[1100px] h-[3px] bg-[#C3C3C3]"></div>
       </div>
 
       <Carousel
