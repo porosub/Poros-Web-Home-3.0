ef6a8c5c9f4a8ef16678732ff3c1514d53024bcf
LeonardTarigan <leontrg32@gmail.com>
2023-11-15 10:29:12 +0700

    menyesuaikan visi-wave-bottom dan visi-sun

--- src/app/tentang-kami/page.tsx
+++ src/app/tentang-kami/page.tsx
@@ -3,7 +3,7 @@ import type { FC } from "react";
 
 const TentangKami: FC = () => {
   return (
-    <main className="min-w-[350px]">
+    <main className="min-w-[350px] overflow-hidden">
       <section className="relative aspect-[3/5] sm:aspect-[3/4] md:aspect-[3/3] lg:aspect-[16/11] w-full px-5 md:px-10 pt-10">
         <Image
           src={"/bg-pengenalan-poros.svg"}
@@ -31,7 +31,7 @@ const TentangKami: FC = () => {
       </section>
       <section className="relative mt-44 pb-20 w-full flex flex-col">
         <div className="relative flex flex-col gap-5 items-center justify-center">
-          <div className="absolute aspect-[16/11] -z-10 w-full min-h-[600px]">
+          <div className="absolute hidden md:block aspect-[16/11] -z-10 md:w-[130%] lg:w-full min-h-[600px]">
             <Image
               src={"/visi-sun.svg"}
               alt="tes"
@@ -39,9 +39,17 @@ const TentangKami: FC = () => {
               className="object-cover"
             />
           </div>
-          <h2 className="text-5xl">VISI</h2>
+          <div className="absolute block md:hidden aspect-square -z-10 sm:w-[150%] md:w-[140%] translate-y-32 md:translate-y-52 min-w-[900px]">
+            <Image
+              src={"/visi-sun-mobile.svg"}
+              alt="tes"
+              fill={true}
+              className="object-cover"
+            />
+          </div>
+          <h2 className="text-3xl sm:text-4xl md:text-5xl">VISI</h2>
 
-          <p className="w-2/3 md:w-1/2 text-center text-xs md:text-base">
+          <p className="w-2/3 md:w-1/2 text-center text-sm md:text-base">
             Mewujudkan POROS yang unggul, berkompeten, dan prestatif dalam
             mengembangkan serta menyebarluaskan pemanfaatan teknologi informasi
             berbasis Linux dan Open Source di lingkungan FILKOM UB.
@@ -53,13 +61,13 @@ const TentangKami: FC = () => {
               src={"/visi-wave-top.svg"}
               alt="tes"
               fill={true}
-              className="-z-10 object-cover"
+              className="-z-10 object-cover translate-y-[1px]"
             />
           </div>
           <div className="bg-[#FF6464] flex flex-col w-full gap-5 justify-center items-center">
-            <h2 className="text-5xl">MISI</h2>
+            <h2 className="text-3xl sm:text-4xl md:text-5xl">MISI</h2>
 
-            <ul className="list-decimal text-xs md:text-base w-2/3">
+            <ul className="list-decimal text-sm md:text-base w-2/3">
               <li>
                 Menjalin kolaborasi dan komunikasi secara aktif dan transparan
                 dengan organisasi atau komunitas, baik internal maupun eksternal
@@ -80,12 +88,12 @@ const TentangKami: FC = () => {
               </li>
             </ul>
           </div>
-          <div className="relative h-20 md:h-44 w-full">
+          <div className="relative h-28 sm:h-36 md:h-48 lg:h-64 xl:h-72 w-full">
             <Image
               src={"/visi-wave-bottom.svg"}
               alt="tes"
               fill={true}
-              className="-z-10 object-cover object-top"
+              className="-z-10 -translate-y-[1px] object-cover object-top"
             />
           </div>
         </div>
